9a5c5a17f30d61d91954b9acb22f84b5c53df756
Rodrigo Serra <rodrigoserrafernandes98@gmail.com>
2018-05-07 00:09:33 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,25 +1,39 @@
 function yellow(){
-	var textColor = document.getElementById('editor');
-	textColor.style.color = "#f4df24";
+	var textColorE = document.getElementById('editor');
+	textColorE.style.color = "#f4df24";
+	var textColorC = document.getElementById('cssEditor');
+	textColorC.style.color = "#f4df24";
+	var textColorS = document.getElementById('scriptEditor');
+	textColorS.style.color = "#f4df24";
 }
 function white(){
-	var textColor = document.getElementById('editor');
-	textColor.style.color = "white";
+	var textColorE = document.getElementById('editor');
+	textColorE.style.color = "white";
+	var textColorC = document.getElementById('cssEditor');
+	textColorC.style.color = "white";
+	var textColorS = document.getElementById('scriptEditor');
+	textColorS.style.color = "white";
 }
 function chooser(){
-	var textColor = document.getElementById('editor');
+	var textColorE = document.getElementById('editor');
 	var color = document.getElementById('userColor').value;
-	textColor.style.color = color;
+	textColorE.style.color = color;
+	var textColorC = document.getElementById('cssEditor');
+	textColorC.style.color = color;
+	var textColorS = document.getElementById('scriptEditor');
+	textColorS.style.color = color;
 	
 }
 
 function update()
 {
-	var idoc = document.getElementById('iframe').contentWindow.document;
-
+	var idoc = document.getElementById('viewer').contentWindow.document;
 	idoc.open();
-	idoc.write(editor.getValue());
+	idoc.write(editor.getValue() + cssEditor.getValue() + scriptEditor.getValue());
 	idoc.close();
+	
+
+	
 }
 
 function setupEditor()
@@ -31,6 +45,7 @@ function setupEditor()
 <body>
 </body>
 </html>`;
+
   window.editor = ace.edit("editor");
   editor.setTheme("ace/theme/monokai");
   editor.getSession().setMode("ace/mode/html");
@@ -45,17 +60,72 @@ function setupEditor()
   
   editor.setOptions({
     fontSize: "14pt",
-    showLineNumbers: false,
-    showGutter: false,
-    vScrollBarAlwaysVisible:true,
+    showLineNumbers: true,
+    showGutter: true,
+    vScrollBarAlwaysVisible:false,
     enableBasicAutocompletion: false, enableLiveAutocompletion: false
   });
 
   editor.setShowPrintMargin(false);
   editor.setBehavioursEnabled(false);
 }
+
+function setupCSSEditor()
+{
+	let text = `<style>
+</style>`;
+	window.cssEditor = ace.edit("cssEditor");
+	cssEditor.setTheme("ace/theme/monokai");
+	cssEditor.getSession().setMode("ace/mode/css");
+	cssEditor.setValue(text,2); 
+	cssEditor.getSession().on('change', function() {
+    update();
+  });
+
+  
+  
+  cssEditor.setOptions({
+    fontSize: "14pt",
+    showLineNumbers: true,
+    showGutter: true,
+    vScrollBarAlwaysVisible:false,
+    enableBasicAutocompletion: false, enableLiveAutocompletion: false
+  });
+
+  cssEditor.setShowPrintMargin(false);
+  cssEditor.setBehavioursEnabled(false);
+}
+
+function setupScriptEditor()
+{
+	let text = `<script>
+</script>`;
+	window.scriptEditor = ace.edit("scriptEditor");
+	scriptEditor.setTheme("ace/theme/monokai");
+	scriptEditor.getSession().setMode("ace/mode/script");
+	scriptEditor.setValue(text,2); 
+	scriptEditor.getSession().on('change', function() {
+    update();
+  });
+
+  
+  
+  scriptEditor.setOptions({
+    fontSize: "14pt",
+    showLineNumbers: true,
+    showGutter: true,
+    vScrollBarAlwaysVisible:false,
+    enableBasicAutocompletion: false, enableLiveAutocompletion: false
+  });
+
+  scriptEditor.setShowPrintMargin(false);
+  scriptEditor.setBehavioursEnabled(false);
+}
+
 function ready()
 {
 setupEditor();
+setupCSSEditor();
+setupScriptEditor();
 update();
 }
